1330b588761f8d4d8065cff389fc98c982d30b59
karimatanysbay-creator <karimatanysbay@gmail.com>
2025-12-28 20:09:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// показываем статические файлы (index.html)
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
-// главная страница
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`BloomMe running on port ${PORT}`);
 });

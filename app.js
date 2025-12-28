62a8cfd983566c0fba3d9b48391cc9224ea447f3
karimatanysbay-creator <karimatanysbay@gmail.com>
2025-12-28 19:31:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {
+  res.send("BloomMe AI is alive 🌸");
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post("/analyze", (req, res) => {
+  const { skin, style } = req.body;
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+  const looks = [
+    {
+      name: "Soft Glam Glow",
+      description: "Сияющая кожа, лёгкий контуринг, розовый блеск",
+      steps: [
+        "Увлажняющий крем",
+        "Лёгкий тон",
+        "Кремовый румянец",
+        "Глянцевые губы"
+      ]
+    },
+    {
+      name: "Clean Chic",
+      description: "Чистая кожа, минимум макияжа, свежесть",
+      steps: [
+        "Праймер",
+        "BB-крем",
+        "Тушь",
+        "Бальзам для губ"
+      ]
+    },
+    {
+      name: "Evening Glam",
+      description: "Вечерний образ с акцентом на губы",
+      steps: [
+        "Матирующая база",
+        "Плотный тон",
+        "Контуринг",
+        "Красная помада"
+      ]
+    }
+  ];
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  res.json({
+    skin,
+    style,
+    recommendations: looks
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("BloomMe AI server running on port " + PORT);
+});

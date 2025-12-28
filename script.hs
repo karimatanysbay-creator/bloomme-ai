29f045360739bf93d58af566650df49ad16c437b
karimatanysbay-creator <karimatanysbay@gmail.com>
2025-12-28 22:57:51 +0500

    Delete script.hs

--- script.hs
+++
@@ -1,7 +0,0 @@
-const startBtn = document.getElementById("startBtn");
-const result = document.getElementById("result");
-
-startBtn.addEventListener("click", () => {
-  result.classList.remove("hidden");
-  startBtn.textContent = "Continue";
-});
